cf341716f2595694670bd9b2c6ebcf1eea0177c6
Sourav Rai <souraviarrai@gmail.com>
2023-07-07 13:49:33 +0600

    new

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,50 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = "Article was successfully created."
+      redirect_to @article
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+  def update
+    @article = Article.find(params[:id])
+    if
+      @article.update(article_params)
+      flash[:notice] = "Article was updated successfully"
+      redirect_to @article
+    else
+      render 'edit', status: :unprocessable_entity
+    end
+  end
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path, status: :see_other
+  end
+
+
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  root "articles#home"
-  resources :articles, only: [:show]
+  root "articles#index"
+  resources :articles
 end
