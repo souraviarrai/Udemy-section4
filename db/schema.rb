7c9ddc97aea9777f1452f2e30ffc6492eebd3826
Sourav Rai <souraviarrai@gmail.com>
2023-07-10 10:50:30 +0600

    added one to many association

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_08_145751) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_10_043711) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -25,6 +26,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_08_145751) do
     t.datetime "updated_at", null: false
   end
 
-
+  create_table "users_table_models", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
