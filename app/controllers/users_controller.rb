ba6a988d1f82ae8a1d7c45c43057e70714ffd1ed
Sourav Rai <souraviarrai@gmail.com>
2023-07-11 15:44:48 +0600

    end of simple authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
-
+  before_action :set_user, only: [:show,:edit,:update]
   def show
-    @user = User.find(params[:id])
     @article = @user.articles.paginate(page: params[:page], per_page: 5)
   end
   def index
@@ -11,10 +10,8 @@ class UsersController < ApplicationController
     @user = User.new
   end
   def edit
-    @user = User.find(params[:id])
   end
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Your account information was successfully updated"
       redirect_to @user
@@ -25,8 +22,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "welcome to the blog #{@user.username}, successfully signed up"
-      redirect_to login_path
+      redirect_to articles_path
     else
       render 'new', status: :unprocessable_entity
     end
@@ -34,6 +32,10 @@ class UsersController < ApplicationController
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
 
